59897b91508ab1d077eae4751133e414b3c3842d
Adrian Banks <github@adrianbanks.co.uk>
2017-02-05 21:37:29 +0000

    Allow the output of speedtest-cli to be passed directly to the site

--- Website/Models/SpeedTestResultRequest.cs
+++ Website/Models/SpeedTestResultRequest.cs
@@ -4,9 +4,24 @@ namespace BroadbandSpeedStats.Models
 {
     public class SpeedTestResultRequest
     {
-        public DateTime DateTime { get; set; }
+        /// <summary>
+        /// The timestamp of when the test was performed.
+        /// </summary>
+        public DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// The ping time in ms.
+        /// </summary>
         public float Ping { get; set; }
+
+        /// <summary>
+        /// The download speed in bit/s.
+        /// </summary>
         public float Download { get; set; }
+
+        /// <summary>
+        /// The upload speed in bit/s.
+        /// </summary>
         public float Upload { get; set; }
     }
 }
