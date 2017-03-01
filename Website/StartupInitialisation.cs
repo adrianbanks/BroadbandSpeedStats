cbf18ea9f8746bf2b6a7cdfe5dbe52c28418bdda
Adrian Banks <github@adrianbanks.co.uk>
2017-03-01 21:15:10 +0000

    Upgrade the database on startup

---
+++ Website/StartupInitialisation.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using BroadbandSpeedTests.Database.Migrations;
+using Nancy.Bootstrapper;
+
+namespace BroadbandSpeedTests.Website
+{
+    public sealed class StartupInitialisation : IApplicationStartup
+    {
+        public void Initialize(IPipelines pipelines)
+        {
+            var connectionStringEntry = ConfigurationManager.ConnectionStrings["default"];
+            var migrator = new Migrator(connectionStringEntry.ConnectionString);
+            migrator.MigrateToLatestSchema();
+        }
+    }
+}
