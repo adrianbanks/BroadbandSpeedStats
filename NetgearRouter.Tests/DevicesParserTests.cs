55ad16de5346f14b217ea09f642d3fd4947a2372
Adrian Banks <github@adrianbanks.co.uk>
2017-03-11 22:29:23 +0000

    Add the ability to parse a list of devices

---
+++ NetgearRouter.Tests/DevicesParserTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using BroadbandStats.NetgearRouter.Parsers;
+using NUnit.Framework;
+using Shouldly;
+
+namespace NetgearRouter.Tests
+{
+    [TestFixture]
+    public sealed class DevicesParserTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("  ")]
+        public void ParsingShouldFailOnInvalidInput(string devicesInformation)
+        {
+            var parser = new DevicesParser();
+            var devices = parser.Parse(devicesInformation);
+            devices.Count().ShouldBe(0);
+        }
+
+        [TestCase("3@")]
+        [TestCase("3@device1")]
+        [TestCase("3@device1@device2")]
+        public void ParsingShouldFailWhenGivenIncompleteInformation(string devicesInformation)
+        {
+            var parser = new DevicesParser();
+            var devices = parser.Parse(devicesInformation);
+            devices.Count().ShouldBe(0);
+        }
+
+        [TestCase("0@@", 0)]
+        [TestCase("1@device1@", 1)]
+        [TestCase("2@device1@device2@", 2)]
+        [TestCase("3@device1@device2@device3@", 3)]
+        public void ParsingShouldReturnTheCorrectNumberOfDevices(string devicesInformation, int expectedNumberOfDevices)
+        {
+            var parser = new DevicesParser();
+            var devices = parser.Parse(devicesInformation);
+            devices.Count().ShouldBe(expectedNumberOfDevices);
+        }
+
+        private const string ExampleInformation = "4@1;192.168.1.2;ANDROID-device;00:00:00:00:00:00;wireless@2;192.168.1.3;BOBS-IPHONE;12:34:56:78:90:AB;wireless@3;192.168.1.4;IPAD;BA:09:87:65:43:21;wireless@4;192.168.1.5;LAPTOP;FF:FF:FF:FF:FF:FF;wireless@";
+    }
+}
---
+++ NetgearRouter/Parsers/DevicesParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BroadbandStats.NetgearRouter.Models;
+
+namespace BroadbandStats.NetgearRouter.Parsers
+{
+    public sealed class DevicesParser
+    {
+        // devices information is in the form:
+        //      count@device information@device information@device information... @
+        //
+        // e.g. 3@device1@device2@device3@
+
+        public IEnumerable<Device> Parse(string devicesInformation)
+        {
+            if (string.IsNullOrWhiteSpace(devicesInformation))
+            {
+                yield break;
+            }
+
+            var parts = devicesInformation.Split(new[] { '@' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 0)
+            {
+                yield break;
+            }
+
+            int deviceCount;
+
+            if (!int.TryParse(parts[0], out deviceCount)
+                || deviceCount != parts.Length - 1)
+            {
+                yield break;
+            }
+
+            var deviceParser = new DeviceParser();
+
+            foreach (var deviceInformation in parts.Skip(1))
+            {
+                yield return deviceParser.Parse(deviceInformation);
+            }
+        }
+    }
+}
