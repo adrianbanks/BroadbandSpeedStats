1358c785d2ea72a77bbea0f4bd39d53bb8097528
Adrian Banks <github@adrianbanks.co.uk>
2017-03-12 01:20:03 +0000

    Split the netgear module into two

---
+++ Website/Modules/Netgear/AttachedDevicesApiModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using BroadbandStats.NetgearRouter.Devices;
+using Nancy;
+using Nancy.Extensions;
+
+namespace BroadbandStats.Website.Modules.Netgear
+{
+    public sealed class AttachedDevicesApiModule : NancyModule
+    {
+        public AttachedDevicesApiModule(AttachedDevicesParser attachedDevicesParser) : base("/netgear")
+        {
+            if (attachedDevicesParser == null)
+            {
+                throw new ArgumentNullException(nameof(attachedDevicesParser));
+            }
+
+            Post["/RecordAttachedDevices"] = _ =>
+            {
+                var requestBody = Request.Body.AsString();
+                var attachedDevices = attachedDevicesParser.Parse(requestBody);
+                return RecordAttachedDevices(attachedDevices);
+            };
+        }
+
+        private HttpStatusCode RecordAttachedDevices(IEnumerable<Device> attachedDevices)
+        {
+            return HttpStatusCode.Created;
+        }
+    }
+}
---
+++ Website/Modules/Netgear/TrafficStatsApiModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BroadbandStats.NetgearRouter.Traffic;
+using Nancy;
+using Nancy.Extensions;
+
+namespace BroadbandStats.Website.Modules.Netgear
+{
+    public sealed class TrafficStatsApiModule : NancyModule
+    {
+        public TrafficStatsApiModule(TrafficStatsParser trafficStatsParser) : base("/netgear")
+        {
+            if (trafficStatsParser == null)
+            {
+                throw new ArgumentNullException(nameof(trafficStatsParser));
+            }
+
+            Post["/RecordTrafficStats"] = _ =>
+            {
+                var requestBody = Request.Body.AsString();
+                var trafficStats = trafficStatsParser.Parse(requestBody);
+                return RecordTrafficStats(trafficStats);
+            };
+        }
+
+        private HttpStatusCode RecordTrafficStats(TrafficStats trafficStats)
+        {
+            return HttpStatusCode.Created;
+        }
+    }
+}
