6579339ca2b7d7d31c73ba1fe12832cac0432ffa
Adrian Banks <github@adrianbanks.co.uk>
2017-03-12 01:11:48 +0000

    Add new tables to the database

---
+++ Database.Migrations/Migrations/6-NetgearStats.cs
@@ -0,0 +1,35 @@
+﻿using BroadbandStats.Database.Schema;
+using FluentMigrator;
+
+namespace BroadbandStats.Database.Migrations.Migrations
+{
+    [Migration(20170312)]
+    public class NetgearStats : Migration
+    {
+        public override void Up()
+        {
+            Create.Table(Tables.AttachedDevices.Name)
+                .WithColumn(Tables.AttachedDevices.Columns.Id).AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn(Tables.AttachedDevices.Columns.Timestamp).AsDateTime().NotNullable()
+                .WithColumn(Tables.AttachedDevices.Columns.DeviceCount).AsInt32().NotNullable();
+
+            Create.Table(Tables.Devices.Name)
+                .WithColumn(Tables.Devices.Columns.Id).AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn(Tables.Devices.Columns.SnapshotId).AsInt32().NotNullable()
+                .WithColumn(Tables.Devices.Columns.DeviceName).AsString(255).NotNullable()
+                .WithColumn(Tables.Devices.Columns.IpAddress).AsString(15).NotNullable()
+                .WithColumn(Tables.Devices.Columns.MacAddress).AsString(17).NotNullable()
+                .WithColumn(Tables.Devices.Columns.ConnectionType).AsString(255).NotNullable();
+
+            Create.ForeignKey()
+                .FromTable(Tables.Devices.Name).ForeignColumn(Tables.Devices.Columns.SnapshotId)
+                .ToTable(Tables.AttachedDevices.Name).PrimaryColumn(Tables.AttachedDevices.Columns.Id);
+        }
+
+        public override void Down()
+        {
+            Delete.Table(Tables.Devices.Name);
+            Delete.Table(Tables.AttachedDevices.Name);
+        }
+    }
+}
--- Database/Schema/Tables.cs
+++ Database/Schema/Tables.cs
@@ -4,7 +4,7 @@
     {
         public static class TestRuns
         {
-           public const string Name = "TestRuns";
+           public const string Name = nameof(TestRuns);
 
             public static class Columns
             {
@@ -25,6 +25,33 @@
                 public const string ServerCountry       = "ServerCountry";
                 public const string ServerCountryCode   = "ServerCountryCode";
                 public const string ServerSponsor       = "ServerSponsor";
+            }
+        }
+
+        public static class AttachedDevices
+        {
+            public const string Name = nameof(AttachedDevices);
+
+            public static class Columns
+            {
+                public const string Id                  = "SnaphostId";
+                public const string Timestamp           = "Timestamp";
+                public const string DeviceCount         = "DeviceCount";
+            }
+        }
+
+        public static class Devices
+        {
+            public const string Name = nameof(Devices);
+
+            public static class Columns
+            {
+                public const string Id                  = "DeviceId";
+                public const string SnapshotId          = "SnaphostId";
+                public const string DeviceName          = "Name";
+                public const string IpAddress           = "IpAddress";
+                public const string MacAddress          = "MacAddress";
+                public const string ConnectionType      = "ConnectionType";
             }
         }
     }
